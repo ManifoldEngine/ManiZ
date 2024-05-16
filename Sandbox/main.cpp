53fd67a3b91f8be1601605d16588b8cb4a2250cb
Nox <nox.tux@gmail.com>
2024-05-16 18:19:15 -0400

    reflection completed

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -20,6 +20,15 @@ int main()
 {
 	Vector v = { .x = 1.0, .y = 2.0, .z = 3.0 };
 	Transform t = { .position = v, .rotation = v, .scale = v };
-	std::cout << ManiZ::toJson(t) << std::endl;
+
+	Transform t2 =
+	{
+		.position = { .x = 5.0, .y = 1000.0, .z = 0.0 },
+		.rotation = {.x = 0.0, .y = 0.0, .z = 0.0 },
+		.scale = {.x = 1.0, .y = 1.0, .z = 1.0 },
+	};
+
+	//std::cout << ManiZ::toJson(t) << std::endl;
+	std::cout << ManiZ::RFL::getFieldName<Vector, 1>() << " = " << t2.position.y << std::endl;
 	return EXIT_SUCCESS;
 }
